028cf529f1c803e02bc920d748c041ccfe7cdcd8
Nachiketa Gargi <nachiketa.gargi@gmail.com>
2017-09-12 10:43:37 -0700

    Cleaned up circle.py

--- circle.py
+++ circle.py
@@ -2,19 +2,24 @@ import cv2
 import numpy as np
 import sys
 
-img = cv2.imread(sys.argv[1],0)
-img = cv2.medianBlur(img,5)
-cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-
-circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
+class CircleFinder():
+	def __init__(self, image_path):
+		self.image_path = image_path
+	def get_center_coords(self):
+		img = cv2.imread(self.image_path,0)
+		img = cv2.medianBlur(img,5)
+		cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+		circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
                             param1=60,param2=30,minRadius=5,maxRadius=300)
+		if circles is None:
+			return ()
+		circles = np.uint16(np.around(circles))
+		max_index = 0
+		for ii, i in enumerate(circles[0,:]):
+			if i[2] > circles[0,max_index,2]: #find largest one
+				max_index = ii
+		return circles[0,max_index,1] # x, y, radius
 
-circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
-    # draw the outer circle
-    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-
-print "x", i[0], "y", i[1], "radius", i[2]
-cv2.imshow('detected circles',cimg)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+	cf = CircleFinder(sys.argv[1])
+	print cf.get_center_coords()
