1ec0a9f73e0c8ad83205ad0e3bb0b74d7fc49d66
Nachiketa Gargi <nachiketa.gargi@gmail.com>
2017-09-18 14:32:10 -0700

    wip

---
+++ star.py
@@ -0,0 +1,23 @@
+# import cv2
+# import numpy as np
+# import sys
+
+
+class StarFinder():
+    def __init__(self):
+        #  TODO: Get image paths?
+        pass
+
+    def find_star(img, search_area):
+        #  img is cv2 image
+        #  search_area is (x, y) for center of ~20 pixel search
+        x = 0
+        y = 0
+        return (x, y)
+
+    def run(self):
+        #  do stuff
+
+
+if __name__ == '__main__':
+    pass
