047488eadc273fd6dce4c194ccc4642ef8d4406e
Nachiketa Gargi <nachiketa.gargi@gmail.com>
2017-09-21 08:49:36 -0700

    wip

--- star.py
+++ star.py
@@ -3,20 +3,21 @@ import time
 # import numpy as np
 import sys
 
+
 class StarFinder():
 
-    recent_xy = ()  #  As far as I can tell, having a class variable is the only way to
-                    #  get the xy val for a click out of the event handler or whatever
+    recent_xy = ()  # As far as I can tell, having a class variable is the only way to
+    #  get the xy val for a click out of the event handler or whatever
 
     def __init__(self, img_path, prev_starfinder=None):
         self.image = cv2.imread(img_path, 0)
         self.compare = prev_starfinder
         if self.compare:
-            self.compare.compare = None  #  Not sure if this is necessary but it's probably best
+            self.compare.compare = None  # Not sure if this is necessary but it's probably best
         #  img_path is the image path to find stars in. The optional
         #  argument prev_starfinder is another Starfinder instance.
-        #  Once stars have been found in one image, pass the 
-        #  Starfinder instance to the new starfinder instance so 
+        #  Once stars have been found in one image, pass the
+        #  Starfinder instance to the new starfinder instance so
         #  that the program can give the user instructions for which
         #  star to click.
         self.star_locs = {}
@@ -40,7 +41,8 @@ class StarFinder():
                 cv2.circle(im, (int(self.compare.star_locs[ky][0] * scale),
                                  int(self.compare.star_locs[ky][1] * scale)), 20, (255, 0, 0), 2)
                 cv2.imshow("comparison", im)
-                cv2.imshow("image", cv2.resize(self.image, (0, 0), fx=scale, fy=scale))
+                cv2.imshow("image", cv2.resize(
+                    self.image, (0, 0), fx=scale, fy=scale))
 
                 key = cv2.waitKey(0) & 0xFF
 
@@ -55,7 +57,8 @@ class StarFinder():
 
         else:
             while True:
-                cv2.imshow("image", cv2.resize(self.image, (0, 0), fx=scale, fy=scale))
+                cv2.imshow("image", cv2.resize(
+                    self.image, (0, 0), fx=scale, fy=scale))
                 key = cv2.waitKey(0) & 0xFF
 
                 if key == ord("y"):
@@ -69,11 +72,12 @@ class StarFinder():
                     break
 
     def find_star(self, search_point, scale):
-        r = 30  #  Half the side length of the search square
+        r = 30  # Half the side length of the search square
         print "find_star running"
-        search_point = (int(search_point[0] / scale), int(search_point[1] / scale))
-        crop_img = self.image[search_point[1] - r : search_point[1] + r, 
-                                search_point[0] - r : search_point[0] + r]
+        search_point = (
+            int(search_point[0] / scale), int(search_point[1] / scale))
+        crop_img = self.image[search_point[1] - r: search_point[1] + r,
+                              search_point[0] - r: search_point[0] + r]
         #  from stackoverflow:
         #  NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]
         x, y = cv2.minMaxLoc(crop_img)[3]
@@ -96,11 +100,13 @@ class StarFinder():
         elif event == cv2.EVENT_LBUTTONUP:
             pass
 
+
 def main():
     finder = StarFinder(sys.argv[1])
     print finder.run()
     finder2 = StarFinder(sys.argv[1], prev_starfinder=finder)
     print finder2.run()
+
 
 if __name__ == '__main__':
     main()
--- stardistance.py
+++ stardistance.py
@@ -1,17 +1,34 @@
 from circle import CircleFinder
+from star import StarFinder
 import cv2
 import sys
+import numpy as np
 
-img = cv2.imread(sys.argv[1],0)
-cf = CircleFinder(sys.argv[1])
-circle_coords = cf.get_center_coords()
-cv2.circle(img,(circle_coords[0],circle_coords[1]),circle_coords[2],(0,255,0),2)
-cv2.circle(img,(circle_coords[0],circle_coords[1]),3,(255,0,0),2)
 
-while True:
-	cv2.imshow('stardistance',img)
-	key = cv2.waitKey(1) & 0xFF
-	if key == ord("q"):
-		break
+class StarDistance():
+    def __init__(self, img1, img2):
+        self.finder1 = StarFinder(img1)
+        self.stars1 = self.finder1.run()
+        self.finder2 = StarFinder(img2, self.finder1)
+        self.stars2 = self.finder2.run()
+        self.img1 = cv2.imread(img1)
+        self.img2 = cv2.imread(img2)
 
-cv2.destroyAllWindows()
+    def run(self):
+        pts1 = np.float32(self.stars1.values())
+        pts2 = np.float32(self.stars2.values())
+        print pts2
+        M = cv2.getAffineTransform(pts1, pts2)
+        rows, cols, ch = self.img1.shape
+        dst = cv2.warpAffine(self.img1, M, (cols, rows))
+        plt.subplot(121), plt.imshow(img), plt.title('Input')
+        plt.subplot(122), plt.imshow(dst), plt.title('Output')
+        plt.show()
+        moonloc1 = CircleFinder(img1).get_center_coords()
+        moonloc2 = CircleFinder(dst).get_center_coords()
+        return (moonloc1[0] - moonloc2[0], moonloc1[1] - moonloc2[1])
+
+
+if __name__ == '__main__':
+    sd = StarDistance(sys.argv[1], sys.argv[2])
+    print sd.run()
