3f9359e7920013b8d13392ecdf15387033733004
Alexander Hughes <alehugh@nuevaschool.org>
2017-10-05 21:40:01 -0700

    made circlefinder actually work

--- circle.py
+++ circle.py
@@ -14,40 +14,45 @@ class CircleFinder():
             self.found_circle = None
 
     def get_center_coords(self):
+        scale = 0.2
+
         if self.image_path is None:
             img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
         else:
             img = cv2.imread(self.image_path, 0)
 
         # Increase exposure
-        img *= 2
-        for row in img:
-            for val in row:
-                if val > 50:
-                    val = 255
-                else:
-                    val = 0
+        thresh = 200
+        img_bw = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)[1]
+        img_bw = cv2.medianBlur(img_bw, 5)
+
+        cv2.imshow("image", cv2.resize(img_bw, (0, 0), fx=scale, fy=scale))
+        cv2.waitKey()
+        cv2.destroyAllWindows()
 
         self.img = img
         img = cv2.medianBlur(img, 5)
 
-        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 2000,
-                                   param1=105, param2=35, minRadius=300,
-                                   maxRadius=700)
-        if circles is None:
-            return None
-        circles = np.uint16(np.around(circles))
-        max_index = 0
-        for ii, i in enumerate(circles[0, :]):
-            if i[2] > circles[0, max_index, 2]:  # find largest one
-                max_index = ii
-        self.show_circles(circles[0])
-        self.found_circle = circles[0, max_index]
+        # Find image centroid
+        M = cv2.moments(img_bw)
+        cx = int(M["m10"] / M["m00"])
+        cy = int(M["m01"] / M["m00"])
+
+        cv2.circle(img, (cx, cy), 7, (0, 0, 0), -1)
+        cv2.imshow("image", cv2.resize(img, (0, 0), fx=scale, fy=scale))
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+        # Compute circle radius with number of white pixels in thresholded image
+        radius = np.sqrt(np.sum(img_bw) / (np.pi * 255))
+
+        self.found_circle = (cx, cy, radius)
+
         return self.found_circle  # x, y, radius
 
+
     def show_circle(self, circle):
-        cv2.circle(
-            self.img, (circle[0], circle[1]), circle[2], (255, 0, 0), 5)
+        cv2.circle(self.img, (circle[0], circle[1]), int(circle[2]), (255, 0, 0), 5)
 
         scale = 0.2
         scaled = cv2.resize(self.img, (0, 0), fx=scale, fy=scale)
@@ -59,7 +64,7 @@ class CircleFinder():
     def show_circles(self, circles):
         for circle in circles:
             cv2.circle(
-                self.img, (circle[0], circle[1]), circle[2], (255, 0, 0), 5)
+                self.img, (circle[0], circle[1]), int(circle[2]), (255, 0, 0), 5)
 
         scale = 0.2
         scaled = cv2.resize(self.img, (0, 0), fx=scale, fy=scale)
